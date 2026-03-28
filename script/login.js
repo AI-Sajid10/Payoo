dc55c98ae1a8ddcec7aa737d4089b4d39192709b
Sajid <c243042@ugrad.iiuc.ac.bd>
2026-03-28 22:49:39 +0600

    login functionality added

---
+++ script/login.js
@@ -0,0 +1,10 @@
+document.getElementById('login-btn').addEventListener('click', function(){
+    const mobileNumber = document.getElementById('mobile-number').value;
+    const pin = document.getElementById('input-pin').value;
+    if(mobileNumber === '01576958290' && pin === '1234'){
+        window.location.assign("/home.html");
+    }
+    else{
+        alert("Login failed. Please check your mobile number and pin.");
+    }
+})
